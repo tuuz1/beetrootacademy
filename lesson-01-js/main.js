50ded7b16ac3e9188289a7336cad8ee1023fdeac
Yaroslav <tuuz121@gmail.com>
2019-11-26 16:19:46 +0200

    add lesson 1 js

---
+++ lesson-01-js/main.js
@@ -0,0 +1,40 @@
+// let name = prompt('Get your name.');
+// alert('hi' + ' ' + name);
+
+// const year = 2019;
+// let age = parseInt(prompt('What your birthday?'));
+// alert(year - age);
+
+// let a = parseInt(prompt('what is a length of side the square'));
+// alert('Perimeter is ' + a * 4);
+
+// let r = parseInt(prompt('radius of circle is...'));
+// alert('Circle area is ' + Math.round(Math.PI * Math.pow(r, 2)));
+
+// let distance = parseInt(prompt('Distance between two cities?'));
+// let time = parseInt(prompt("and hours"));
+// alert('Your speed ' + Math.round(distance / time));
+
+// const euro = 0.91;
+// let dollar = parseInt(prompt('dollars: '));
+// alert('euro ' + dollar * euro);
+
+// const value = parseInt(prompt('value your card: '));
+// alert(Math.floor(value / .82) + ' files');
+
+// let money = parseInt(prompt('your money are: '));
+// let price = parseInt(prompt('price of chocolate'));
+// let residual = money % price;
+// alert(Math.floor(money / price) + ', ' + residual);
+
+// let number = prompt('Your number: ');
+// let reverse = 0;
+// const x = String(number);
+// while (number > 0) {
+//     reverse = reverse * 10 + number % 10;
+//     number = Math.floor(number / 10);
+// }
+// alert(reverse);
+
+// let deposit = prompt('Your deposit:');
+// alert(Math.round(deposit = deposit + deposit * (.05 / 12 * 2)));
